0aebf4bf7e8028b7abb97e75a4a0888cde14356e
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-04-02 12:16:32 +0100

    installed sanity studio

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <main className="h-screen text-white bg-gradient-to-br from-blue-600 to-purple-700">
       <Head>
         <title>Mira NFT Marketplace</title>
         <link rel="icon" href="/images/nft.png" />
       </Head>
 
       <h2>Mira NFT Marketplace</h2>
-    </div>
+    </main>
   )
 }
 
