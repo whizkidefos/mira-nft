65b25cedaaa3ff1c4e1637ce256cf08ce3304122
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-04-04 14:44:52 +0100

    added collection interface and typings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,85 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
+import { sanityClient, urlFor } from '../sanity';
+import { Collection } from '../typings';
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection[];
+}
+
+const Home = ({ collections }: Props) => {
+  
+
   return (
-    <main className="h-screen text-white bg-gradient-to-br from-blue-600 to-purple-700">
+    <main className="min-h-screen px-10 py-20 text-white mi bg-gradient-to-br from-blue-600 to-purple-700">
       <Head>
         <title>Mira NFT Marketplace</title>
         <link rel="icon" href="/images/nft.png" />
       </Head>
 
-      <h2>Mira NFT Marketplace</h2>
+      <header className='flex items-center justify-between'>
+        <h1 className='text-xl cursor-pointer w-52 sm:w-80 font-extralight'>Mira NFT Marketplace</h1>
+        {/* <button onClick={() => (address ? disconnect() : connectWithMetamask())} className='px-4 py-2 text-xs text-white bg-purple-600 rounded-full lg:px-5 lg:py-3 lg:text-base hover:bg-purple-700'>{address ? 'Sign Out' : 'Sign In'}</button> */}
+      </header>
+      <hr className='my-3' />
+
+      <section className='p-10 mx-auto shadow-rose-400 max-w-7xl'>
+        <div className='grid space-x-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
+          {collections.map(collection => (
+            <Link href={`/nft/${collection.slug.current}`}>
+              <div key={collection._id} className="flex flex-col items-center transition-all duration-200 cursor-pointer hover:scale-105">
+                <img className='object-cover h-96 rounded-2xl' src={urlFor(collection.mainImage).url()} alt={collection.title} />
+
+                <article className='p-5'>
+                  <h2 className="text-xl">{collection.title}</h2>
+                  <p className="mt-2 text-sm text-gray-100">{collection.description}</p>
+                </article>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
     </main>
   )
 }
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = `*[_type == 'collection'] {
+        _id,
+        title,
+        address,
+        description,
+        nftCollectionName,
+        mainImage {
+        asset
+      },
+      previewImage {
+        asset
+      },
+      slug {
+        current
+      },
+      creator-> {
+        _id,
+        name,
+        address,
+        slug {
+        current
+      },
+    },
+  }`
+
+  const collections = await sanityClient.fetch(query);
+  console.log(collections)
+
+  return {
+    props: {
+      collections,
+    }
+  }
+
+}
--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { GetServerSideProps } from 'next';
 import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import { sanityClient, urlFor } from '../../sanity';
+import { Collection } from '../../typings';
+import Link from 'next/link';
 
-const NFTDropPage = () => {
+interface Props {
+  collection: Collection
+}
+
+const NFTDropPage = ({ collection }: Props) => {
   // Auth hooks
   const connectWithMetamask = useMetamask();
   const address = useAddress();
@@ -15,21 +23,24 @@ const NFTDropPage = () => {
         <div className='lg:col-span-4 bg-gradient-to-br from-blue-600 to-purple-700'>
             <div className='flex flex-col items-center justify-center py-2 lg:min-h-screen'>
                 <figure className='p-2 bg-gradient-to-br from-yellow-400 to-purple-600 rounded-xl'>
-                  <img src='/images/1.png' alt='' className='object-cover w-44 rounded-xl lg:h-96 lg:w-72' />
+                  <img src={urlFor(collection.previewImage).url()} alt='' className='object-cover w-44 rounded-xl lg:h-96 lg:w-72' />
                 </figure>
 
                 <div className='p-5 space-y-2 text-center'>
-                    <h1 className="text-4xl font-bold text-white">MiraNFT Cruise</h1>
-                    <h2 className="text-xl text-gray-300">A collection of Mira Cruise for travel in the metaverse</h2>
+                    <h1 className="text-4xl font-bold text-white">{collection.nftCollectionName}</h1>
+                    <h2 className="text-xl text-gray-300">{collection.description}</h2>
                 </div>
             </div>
         </div>
 
         {/* Right */}
         <div className='flex flex-col flex-1 h-screen p-12 lg:col-span-6'>
+
           {/* header */}
           <header className='flex items-center justify-between'>
-            <h1 className='text-xl cursor-pointer w-52 sm:w-80 font-extralight'>Mira NFT Marketplace</h1>
+            <Link href="/">
+              <h1 className='text-xl cursor-pointer w-52 sm:w-80 font-extralight'>Mira NFT Marketplace</h1>
+            </Link>
             <button onClick={() => (address ? disconnect() : connectWithMetamask())} className='px-4 py-2 text-xs text-white bg-purple-600 rounded-full lg:px-5 lg:py-3 lg:text-base hover:bg-purple-700'>{address ? 'Sign Out' : 'Sign In'}</button>
           </header>
           <hr className='my-3' />
@@ -38,9 +49,9 @@ const NFTDropPage = () => {
 
           {/* content */}
           <div className='flex flex-col items-center flex-1 mt-10 space-y-6 text-center lg:space-y-0 lg:justify-center'>
-            <img className='object-cover pb-10 w-96 lg:h-72' src='/images/pixmike.jpg' alt='nft mosaic' />
-            <h2 className="text-2xl font-bold lg:text-5xl lg:font-extrabold">Some title for this copy | MiraNFT</h2>
-            <p className="pt-2 text-xl text-green-500">13 / 21 NFT's claimed</p>
+            <img className='object-cover w-full pb-10 rounded-lg lg:h-96' src={urlFor(collection.mainImage).url()} alt='nft mosaic' />
+            <h2 className="text-xl font-bold text-gray-700 lg:text-2xl lg:font-extrabold">{collection.title}</h2>
+            <p className="pt-2 text-green-500 text-md">13 / 21 NFT's claimed</p>
           </div>
 
           {/* mint button */}
@@ -50,4 +61,47 @@ const NFTDropPage = () => {
   )
 }
 
-export default NFTDropPage
+export default NFTDropPage;
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const query = `*[_type == 'collection' && slug.current == $id][0] {
+        _id,
+        title,
+        address,
+        description,
+        nftCollectionName,
+        mainImage {
+        asset
+      },
+      previewImage {
+        asset
+      },
+      slug {
+        current
+      },
+      creator-> {
+        _id,
+        name,
+        address,
+        slug {
+        current
+      },
+    },
+  }`
+
+  const collection = await sanityClient.fetch(query, {
+    id: params?.id,
+  });
+
+  if (!collection) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      collection
+    }
+  }
+}
